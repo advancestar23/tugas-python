2eec787aeaedff1978388fcc42b9d9958258dd59
advancestar23 <113220596+advancestar23@users.noreply.github.com>
2022-09-10 10:24:21 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,39 @@
+# def fungsi_python():
+#     print("holla")
+
+
+# fungsi python
+# fungsi_python()
+
+# nama = "ali"
+# umur = 18
+# print("nama saya adalah ", nama ,"umur saya adalah ", str(umur))
+
+# ukuransepatu = 43.5
+# print("ukuran sepatu saya", ukuransepatu, type(ukuransepatu))
+
+# masukkelas = True
+# pulangkelas = False
+# print(masukkelas, type(masukkelas))
+
+# hewan = ["ayam", "kelinci", "kambing", "tupai"]
+# hewalaut = ("mas", "nemo", "paus", "hiu")
+# hewandarat = {"kucing", "harimau"}
+# print (hewan[1])
+
+from unicodedata import name
+
+
+angka1 = 5
+angka2 = 10
+angka3 = 15
+
+print("penjumlahan", angka1+angka2+angka3+15)
+print("pengurangan", angka1-angka2-angka3-15)
+print("pembagian", angka1/angka2/angka3/15)
+print("pembagian 2", 63//10)
+print("perkalian", angka1*angka2)
+print("modulus", 75%10)
+
+name = input("masukkan nama : ")
+print(name)
